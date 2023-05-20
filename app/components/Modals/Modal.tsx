a4242a75090549dd73443c46fa5680ffe2ae32c5
hadar <hadar.zaguri@gmail.com>
2023-05-21 00:38:39 +0300

    work on Modal.tsx

---
+++ app/components/Modals/Modal.tsx
@@ -0,0 +1,96 @@
+'use client'
+import { useCallback, useEffect, useState } from "react";
+
+interface ModalProps{
+    isOpen?: boolean;
+    onClose:()=> void;
+    onSubmit:()=>void;
+    title? : string;
+    body? : React.ReactElement;
+    footer? : React.ReactElement;
+    actionLabel: string;
+    disabled? : boolean;
+    secondaryAction?: () => void;
+    secondaryLabel?: string;
+}
+const Modal : React.FC<ModalProps> = ({
+    isOpen,
+    onClose,
+    onSubmit,
+    title,
+    body,
+    footer,
+    actionLabel,
+    disabled,
+    secondaryAction,
+    secondaryLabel
+}) => {
+    const [showModal,setShowModal]= useState(isOpen)
+    
+    useEffect(()=>{
+        setShowModal(isOpen)
+    },[])
+
+    const handleClose = useCallback(()=>{
+        if(disabled) return;
+        setShowModal(false)
+
+        setTimeout(() => {
+            onClose();
+        }, 300);
+    },[isOpen,onclose])
+
+    const handleSubmit = useCallback(()=>{
+        if(disabled) return;
+
+        onSubmit();
+    },[disabled,onSubmit])
+
+    const handleSecondaryAction = useCallback(()=>{
+        if(disabled || !secondaryAction) return;
+
+        secondaryAction();
+    },[disabled,secondaryAction])
+
+    if(!open)
+        return null;    
+    return (<>
+    <div
+        className="
+            justify-center
+            items-center
+            flex
+            overflow-x-hidden
+            overflow-y-hidden
+            fixed
+            inset-0
+            z-50
+            outline-none
+            focus:outline-none
+            bg-neutral-800/70
+
+        ">
+        <div className="
+            transalte
+            h-dull
+            lg:h-auto
+            md:h-auto
+
+            border-0
+            rounded-lg
+            shadow-lg
+            flex
+            flex-col
+            w-full
+            bg-white
+            outline-none
+            foux:outline-none">
+        </div>
+
+    </div>
+
+    </>
+    );
+}
+ 
+export default Modal;
